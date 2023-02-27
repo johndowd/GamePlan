83c5480c1c55bfb96e6b5589892848c603763af3
Jack Dowd <johnmanningdowd@gmail.com>
2023-02-27 12:02:49 -0500

    added games sorted by most recent

--- client/src/components/games/GamesList.js
+++ client/src/components/games/GamesList.js
@@ -8,6 +8,11 @@ const GamesList = (props) => {
     try {
       const response = await fetch(`/api/v1/games`)
       const body = await response.json()
+      const sortedGames = body.games.sort((game1, game2) => {
+        const date1 = new Date(game1.date)
+        const date2 = new Date(game2.date)
+        return (date1 > date2 ? 1 : -1)
+      })
       setGames(body.games)
     } catch (error) {
       console.error(error)
@@ -24,6 +29,7 @@ const GamesList = (props) => {
 
   return (
     <div className='game-list-page'>
+      <h2>Recent Games</h2>
       <ul className='game-list'>
         {gameTiles}
       </ul>
