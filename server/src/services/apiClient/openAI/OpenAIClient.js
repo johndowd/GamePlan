42a7082b86eb2c69f5fe5c8be0d458ed1f221864
Jack Dowd <johnmanningdowd@gmail.com>
2023-04-06 17:57:42 -0400

    got generation working with aws

--- server/src/services/apiClient/openAI/OpenAIClient.js
+++ server/src/services/apiClient/openAI/OpenAIClient.js
@@ -1,4 +1,5 @@
 import { Configuration, OpenAIApi } from "openai";
+import { uploadB64 } from "../../AWS/uploadB64.js";
 
 class OpenAIClient {
   constructor() {
@@ -12,7 +13,7 @@ class OpenAIClient {
     const response = await this.openai.createCompletion({
       "model": "text-davinci-003",
       "n": 1,
-      "prompt": "come up with a username"
+      "prompt": "come up with a username that is less than 25 characters"
     })
     const { choices } = response.data
     const username = choices[0].text
@@ -21,12 +22,13 @@ class OpenAIClient {
 
   async generateProfileImage(prompt) {
     const response = await this.openai.createImage({
-      prompt: `create a profile picture for ${prompt}`
+      prompt: `create a profile picture for ${prompt}`,
+      response_format: 'b64_json'
     })
 
     const { data } = response.data
-    const { url } = data[0]
-    return url
+    const loc = await uploadB64(data[0]?.b64_json)
+    return loc
   }
 }
 
